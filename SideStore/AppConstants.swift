f419b783836ff9a18e260f366a51abd57a402fb5
joeshu <350027830@qq.com>
2026-09-16 12:56:16 +0800

    Prefer automatic VPN device endpoint discovery

--- SideStore/AppConstants.swift
+++ SideStore/AppConstants.swift
@@ -23,14 +23,9 @@ public enum AppConstants {
     }
     
     public enum Connection {
-        //fix: temporarily reverting the no default overrideIP 
-        //     coz though our auto discovery works perfectly, 
-        //     localDevVPN is flawed in its routing table 
-        //   - so until it is fixed, to reduce friction we are okay with this
-        //
-        // when localdevvpn is fixed, we can comment out the line with an IP with with the one with ""
-        public static let defaultOverrideIP = "10.7.0.1"
-        // public static let defaultOverrideIP = ""    // auto-discover is robust we dont need to supply default
+        // Prefer route/utun auto-discovery. Users with a fixed VPN endpoint
+        // can still enter a manual Device IP in Connection settings.
+        public static let defaultOverrideIP = ""
         public static let defaultRemoteServerIP = "10.7.0.1"
     }
     
